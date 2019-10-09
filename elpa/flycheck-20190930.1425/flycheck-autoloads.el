7460ba64b54ec030c432dd4623e45ae952f6fea6
Yun <yun26603020@gmail.com>
2019-10-09 00:27:04 -0400

    AutoPep8 and Syntax Checking
    replaced flymake with flycheck, added autopep8

--- elpa/flycheck-20190930.1425/flycheck-autoloads.el
+++ elpa/flycheck-20190930.1425/flycheck-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (23963 63053 985972
-;;;;;;  126000))
+;;;### (autoloads nil "flycheck" "flycheck.el" (23965 24544 541014
+;;;;;;  881000))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-manual "flycheck" "\
@@ -228,7 +228,7 @@ Use this together with the `option', `option-list' and
 ;;;***
 
 ;;;### (autoloads nil nil ("flycheck-buttercup.el" "flycheck-ert.el"
-;;;;;;  "flycheck-pkg.el") (23963 63053 981972 125000))
+;;;;;;  "flycheck-pkg.el") (23965 24544 537014 888000))
 
 ;;;***
 
