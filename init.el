6b28ac04099336627583bd4a670f1fae8bea63ce
Yun <yun26603020@gmail.com>
2020-10-22 19:39:33 -0400

    remove custom set variables and faces

--- init.el
+++ init.el
@@ -326,18 +326,6 @@
 
 ;; disable flycheck for this file
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((eval visual-line-mode t))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(elpy-folding-fringe-face ((t (:inherit 'font-lock-keyword-face :box (:line-width 1 :style released-button))))))
 ;; Local Variables:
 ;; flycheck-disabled-checkers: emacs-lisp-checkdoc
 ;; End:
